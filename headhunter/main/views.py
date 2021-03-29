be6da91285bc56c9980cf194da6dce038831d8b2
Pavel Olifer <pavel.olifer@gmail.com>
2021-03-29 06:26:03 +0300

    add turn_on_block variable

--- headhunter/main/views.py
+++ headhunter/main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'turn_on_block': True,
+    }
+    return render(request, 'index.html', context=context)
