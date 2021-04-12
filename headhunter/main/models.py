e98c1e2c68525d6f9b9ac1bcb066f1e12f88f665
Pavel Olifer <pavel.olifer@gmail.com>
2021-04-12 04:03:09 +0300

    put common_users group after user creation

--- headhunter/main/models.py
+++ headhunter/main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.dispatch import receiver
 
 from sorl.thumbnail import ImageField
@@ -164,3 +164,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        instance.groups.add(Group.objects.get(name='common_users'))
