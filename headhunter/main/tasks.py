c996ad72b7f72b6cfe1456f4879d66bccc337c2d
Pavel Olifer <pavel.olifer@gmail.com>
2021-04-20 01:40:14 +0300

    add sms sending task

--- headhunter/main/tasks.py
+++ headhunter/main/tasks.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timedelta
 import logging
+import random
 
 from django.core.mail import EmailMultiAlternatives
 from celery.utils.log import get_task_logger
 from celery import shared_task
 
-from .models import Vacancy, Subscriber
+from .models import Vacancy, Subscriber, SMSLog
 from headhunter.celery import app
 
 
@@ -43,3 +44,35 @@ def get_new_vacancy():
 def get_new_vacancy_task():
     logger.info('Sent new vacancy lists.')
     return get_new_vacancy
+
+
+@shared_task
+def create_sms_task():
+    logger.info('Sending sms')
+    sms_code = random.sample([str(x) for x in range(10)], 4)
+    sms_code = ''.join(sms_code)
+    cell_phone = '79219935443'
+
+    import vonage
+
+    client = vonage.Client(key="999f0f38", secret="yd8FzNA1zaqOY2bj")
+    sms = vonage.Sms(client)
+
+    responseData = sms.send_message(
+        {
+            "from": "Vonage APIs",
+            "to": cell_phone,
+            "text": sms_code,
+        }
+    )
+
+    server_responde = responseData["messages"][0]["status"]
+
+    a = SMSLog(phone_number=cell_phone, code=sms_code,
+               server_response=server_responde).save()
+
+    if server_responde == "0":
+        logger.info('Message sent successfully.')
+    else:
+        error_msg = responseData['messages'][0]['error-text']
+        logger.info(f"Message failed with error: {error_msg}")
